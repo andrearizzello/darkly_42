a92db4a2b236009bf36edad505f6a3781c059dcb
Andrea Rizzello <andrea@andrew.local>
2019-07-13 16:55:45 +0200

    Web scraping

---
+++ web_scraping/Resources/main.py
@@ -0,0 +1,14 @@
+import re
+from urllib.request import urlopen
+
+with open('sitemap.txt') as sitemap:
+    line = sitemap.readline()
+    while line:
+        with urlopen(line) as response:
+            html = response.read().decode('utf-8')
+            lst = re.findall(r"([a-fA-F\d]{32})", html)
+            if lst:
+                print('Here is the flag ' + lst[0])
+                print(line)
+                exit(0)
+        line = sitemap.readline()
